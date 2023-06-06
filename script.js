d835a4316337f82faab6387d0ea131f16f0d18fb
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-06 16:28:44 +0200

    function corrected and completed

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ const userWord = prompt('Enter a word:');
 // function to determine if word is palindrome
 function isPalindrome(word) {
     // Reverse the word
-  const reversedWord = formattedWord.split('').reverse().join('');
-  // Check if the reversed word is equal to the original word
-  return formattedWord === reversedWord;
+    const reversedWord = userWord.split('').reverse().join('');
+    // Check if the reversed word is equal to the original word
+    return userWord === reversedWord;
+}
+
+// I get the result
+const result = isPalindrome(userWord);
+
+// if cycle to see if is palyndrome or not
+if (result) {
+  console.log(`${userWord} is a palindrome.`);
+} else {
+  console.log(`${userWord} is not a palindrome.`);
 }
