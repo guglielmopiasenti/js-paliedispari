5f6d875566169b5c03f07756d4a612ea4a89b428
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-06 16:06:39 +0200

    js palindrome function

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const userWord = prompt('Enter a word:');
 
 // function to determine if word is palindrome
 function isPalindrome(word) {
-    
+    // Reverse the word
+  const reversedWord = formattedWord.split('').reverse().join('');
+  // Check if the reversed word is equal to the original word
+  return formattedWord === reversedWord;
 }
